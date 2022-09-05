94db45a6d1b1997dd5596f1a1e30939c5b4820b1
andrflo <andrewflorezrod@gmail.com>
2022-09-05 17:50:41 -0500

    black

--- calculator.py
+++ calculator.py
@@ -1,15 +1,14 @@
 from libmate import square
 
-def main():
-    x=input("What is x? ")
-    print("x squared is", square(float(x)))
-    y=input("What is y? ")
 
-    z=round(float(x)/float(y), 2)
+def main():
+    x = input("What is x? ")
+    print("x squared is", square(float(x)))
+    y = input("What is y? ")
+
+    z = round(float(x) / float(y), 2)
 
     print(f"{z:,}")
-    
+
+
 main()
-
-
-
