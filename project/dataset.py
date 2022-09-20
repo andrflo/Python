62ae913dbc33d3cf687e1be074a0ea2504e8e332
andrflo <andrewflorezrod@gmail.com>
2022-09-20 22:59:04 +0000

    Sep 20, 2022, 5:59 PM

--- project/dataset.py
+++ project/dataset.py
@@ -403,7 +403,7 @@ class Dataset:
                                                 if days_service > 0 and len(row[param]) > 0:
                                                     x2.append(days_service)
                                                     y2.append(float(row[param]))
-                                                    j += 1
+                                                j += 1
                                         elif len(row["Einfülltage"]) > 0:
                                             x2.append(int(row["Einfülltage"]))
                                             y2.append(float(row[param]))
@@ -429,7 +429,7 @@ class Dataset:
                                             if days_service > 0 and len(row[param]) > 0:
                                                 x3.append(days_service)
                                                 y3.append(float(row[param]))
-                                                j += 1
+                                            j += 1
                                     elif len(row["Einfülltage"]) > 0:
                                         x3.append(int(row["Einfülltage"]))
                                         y3.append(float(row[param]))
@@ -453,8 +453,8 @@ class Dataset:
                                             if days_service > 0 and len((data[i])[param]) > 0:
                                                 x1.append(days_service)
                                                 y1.append(float((data[i])[param]))
-                                                j += 1
-                                    elif len(row["Einfülltage"]) > 0:
+                                            j += 1
+                                    elif len((data[i])["Einfülltage"]) > 0:
                                         x1.append(int((data[i])["Einfülltage"]))
                                         y1.append(float((data[i])[param]))
                                         j += 1
@@ -485,7 +485,7 @@ class Dataset:
                                     if days_service > 0 and len((data[i])[param]) > 0:
                                         x1.append(days_service)
                                         y1.append(float((data[i])[param]))
-                            elif len(row["Einfülltage"]) > 0:
+                            elif len((data[i])["Einfülltage"]) > 0:
                                 x1.append(int((data[i])["Einfülltage"]))
                                 y1.append(float((data[i])[param]))
                             i += 1
