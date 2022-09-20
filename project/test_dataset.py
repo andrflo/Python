6ecf7e9d1fbe1b2382433d2aae43ead56c693d61
andrflo <andrewflorezrod@gmail.com>
2022-09-20 03:00:06 +0000

    Sep 19, 2022, 10:00 PM

--- project/test_dataset.py
+++ project/test_dataset.py
@@ -3,4 +3,4 @@ from dataset import Dataset
 def test_keys_exist():
     fn1 = "dataset1.csv"
     ds = Dataset(fn1)
-    assert ds.keys_exist("Anlagennummer")
+    assert ds.keys_exist("Anlagennummer", "Wasser K. F.")
