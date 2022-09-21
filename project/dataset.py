ebbc1315c9b3e9e5d1b7d4cf6d04db1c8e03cee5
andrflo <andrewflorezrod@gmail.com>
2022-09-21 05:34:15 +0000

    Sep 21, 2022, 12:34 AM

--- project/dataset.py
+++ project/dataset.py
@@ -285,8 +285,11 @@ class Dataset:
                 for row1 in reader:
                     if row1["Ölbezeichnung"] == oil_name:
                         short_ds.append(row1)  
-                        
-            print(short_ds[0])                 
+            u=1            
+            for d in short_ds:
+                print(d["Ölbezeichnung"])
+                print(u)
+                u+=1                 
             return sorted(short_ds, key=lambda row: (row[param]))
             
     def set_of_oils(self, source, season, param):
