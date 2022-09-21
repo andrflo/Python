5b76de9a5a4ded453ff328dc7f23361591fce363
andrflo <andrewflorezrod@gmail.com>
2022-09-21 00:27:04 +0000

    Sep 20, 2022, 7:27 PM

--- project/dataset.py
+++ project/dataset.py
@@ -383,7 +383,7 @@ class Dataset:
                     if (data[i])["Ölbezeichnung"] == oil_name and self.origin_sample((data[i])["Probe aus"], "wind", "wea", "wka", "éolienne"):
                         machine_id = (data[i])[a]
                         nop = res[machine_id]
-                        #print(i, machine_id, nop)
+                        print(i, machine_id, nop)
                         if nop > 3 and (not first_done or not second_done):
                             j = 0
                             s = 0
