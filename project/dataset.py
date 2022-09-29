baaccf88733deb4d2cd0475fb824b5e928e34bcf
andrflo <andrewflorezrod@gmail.com>
2022-09-29 23:51:24 +0000

    Sep 29, 2022, 6:51 PM

--- project/dataset.py
+++ project/dataset.py
@@ -255,29 +255,29 @@ class Dataset:
                         
                     ax_histy_summer.hist(d1["y_values"], bins=bins, color="red", orientation='horizontal')
 
-                if len(d2["x_values"]) > 0:
-                    xymax = max(np.max(np.abs(d2["x_values"])), np.max(np.abs(d2["y_values"])))
+                if len(d1["x_values"]) > 0:
+                    xymax = max(np.max(np.abs(d1["x_values"])), np.max(np.abs(d1["y_values"])))
                     lim = (int(xymax/binwidth) + 1) * binwidth
 
                     bins = np.arange(-lim, lim + binwidth, binwidth)
                         
-                    ax_histy_fall.hist(d2["y_values"], bins=bins, color="m", orientation='horizontal')
+                    ax_histy_fall.hist(d1["y_values"], bins=bins, color="m", orientation='horizontal')
 
-                if len(d3["x_values"]) > 0:
-                    xymax = max(np.max(np.abs(d3["x_values"])), np.max(np.abs(d3["y_values"])))
+                if len(d1["x_values"]) > 0:
+                    xymax = max(np.max(np.abs(d1["x_values"])), np.max(np.abs(d1["y_values"])))
                     lim = (int(xymax/binwidth) + 1) * binwidth
 
                     bins = np.arange(-lim, lim + binwidth, binwidth)
                         
-                    ax_histy_winter.hist(d3["y_values"], bins=bins, color="blue", orientation='horizontal')
+                    ax_histy_winter.hist(d1["y_values"], bins=bins, color="blue", orientation='horizontal')
 
-                if len(d4["x_values"]) > 0:
-                    xymax = max(np.max(np.abs(d4["x_values"])), np.max(np.abs(d4["y_values"])))
+                if len(d1["x_values"]) > 0:
+                    xymax = max(np.max(np.abs(d1["x_values"])), np.max(np.abs(d1["y_values"])))
                     lim = (int(xymax/binwidth) + 1) * binwidth
 
                     bins = np.arange(-lim, lim + binwidth, binwidth)
                         
-                    ax_histy_spring.hist(d4["y_values"], bins=bins, color="green", orientation='horizontal')
+                    ax_histy_spring.hist(d1["y_values"], bins=bins, color="green", orientation='horizontal')
 
 
 
