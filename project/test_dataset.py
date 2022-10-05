2acfb109296a2f6e9c495d5ab3815d89e9c9f40c
andrflo <andrewflorezrod@gmail.com>
2022-10-05 22:34:57 +0000

    Oct 5, 2022, 5:34 PM

--- project/test_dataset.py
+++ project/test_dataset.py
@@ -4,3 +4,9 @@ def test_keys_exist():
     fn1 = "dataset1.csv"
     ds = Dataset(fn1)
     assert ds.keys_exist("Anlagennummer", "Wasser K. F.")
+
+def test_sort_by_param():
+    fn3 = "dataset3.csv"
+    ds = Dataset(fn3)
+    oil_names = ds.set_of_oils("wind turbine", "all_seasons", "Viskosität bei 100°C")
+    data = ds.sort_by_param("Probenbezeichnung", oil_names, "wind turbine")
