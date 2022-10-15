6c707c8ed2ec767e8f9038934602595136355d2b
andrflo <andrewflorezrod@gmail.com>
2022-10-15 18:07:30 +0000

    Oct 15, 2022, 1:07 PM

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -131,7 +131,7 @@ def shortest_path(source, target):
 
         # Add neighbors to frontier
         for movie, actor in neighbors_for_person(source):
-            if not frontier.contains_state(actor) and actor not in self.explored:
+            if not frontier.contains_state(actor) and actor not in explored:
                 child = Node(state=actor, parent=node, action=movie)
                 frontier.add(child)
 
