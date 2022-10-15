f8f5e690cb64b579d7bdced856f05f97755cc4fa
andrflo <andrewflorezrod@gmail.com>
2022-10-15 20:18:05 +0000

    Oct 15, 2022, 3:18 PM

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -133,7 +133,7 @@ def shortest_path(source, target):
 
         # Mark node as explored
         explored.add(node.state)
-        if (node.state == '1270798'):
+        if (node.state == '102'):
             print("kb visitado")
 
         # Add neighbors to frontier
