f2a6b9e6b788976f80ec530b3ab71bcd1c81ad3d
andrflo <andrewflorezrod@gmail.com>
2022-10-15 18:02:50 +0000

    Oct 15, 2022, 1:02 PM

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -126,20 +126,19 @@ def shortest_path(source, target):
             return (movies, actors)
             
 
-            # Mark node as explored
-            explored.add(node.state)
+        # Mark node as explored
+        explored.add(node.state)
 
-            # Add neighbors to frontier
-            for action, state in self.neighbors(node.state):
-                if not frontier.contains_state(state) and state not in self.explored:
-                    child = Node(state=state, parent=node, action=action)
-                    frontier.add(child)
+        # Add neighbors to frontier
+        for movie, actor in neighbors_for_person(source):
+            if not frontier.contains_state(state) and state not in self.explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
 
 
-    neighbors_for_person(source)
+    
 
-    # TODO
-    raise NotImplementedError
+    
 
 
 def person_id_for_name(name):
