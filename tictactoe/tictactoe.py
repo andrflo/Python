1f3b72879f0d41cb5f1300873a619ad84036184e
andrflo <andrewflorezrod@gmail.com>
2022-10-17 02:31:59 +0000

    Oct 16, 2022, 9:31 PM

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -51,6 +51,7 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    print(action)
     if not (0 <= action[0] < 3) or not (0 <= action[1] < 3):
         raise Exception("Invalid action")
     if board[action[0]][action[1]] != EMPTY:
