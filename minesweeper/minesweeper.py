ab59161b492ef8771f315a33f18878c379ff6bf8
andrflo <andrewflorezrod@gmail.com>
2022-10-19 18:33:08 +0000

    Oct 19, 2022, 1:33 PM

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -105,20 +105,24 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells)==1 and self.count==1:
+            return self.cells
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if len(self.cells)==1 and self.count==0:
+            return self.cells
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells and self.count > 0:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
