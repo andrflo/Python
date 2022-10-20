262fe0dc6449e249e9007e1cba706da7b0fa1069
andrflo <andrewflorezrod@gmail.com>
2022-10-20 00:00:59 +0000

    Oct 19, 2022, 7:00 PM

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -240,8 +240,7 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
-
+        
     def make_random_move(self):
         """
         Returns a move to make on the Minesweeper board.
