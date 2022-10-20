acd10bb681f6e09734d7ca58c377d657358cfb64
andrflo <andrewflorezrod@gmail.com>
2022-10-20 00:49:52 +0000

    Oct 19, 2022, 7:49 PM

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -194,21 +194,21 @@ class MinesweeperAI():
         list_of_cells = []
         for i in range(self.height):
             for j in range(self.width):
-                if self.withinboard(i+1, j-1) and not(self.identified((i+1, j-1))):
+                if self.withinboard((i+1, j-1)) and not(self.identified((i+1, j-1))):
                     list_of_cells.append((i+1, j-1))
-                if self.withinboard(i+1, j) and not(self.identified((i+1, j))):
+                if self.withinboard((i+1, j)) and not(self.identified((i+1, j))):
                     list_of_cells.append((i+1, j))  
-                if self.withinboard(i+1, j+1) and not(self.identified((i+1, j+1))):
+                if self.withinboard((i+1, j+1)) and not(self.identified((i+1, j+1))):
                     list_of_cells.append((i+1, j+1)) 
-                if self.withinboard(i, j-1) and not(self.identified((i, j-1))):
+                if self.withinboard((i, j-1)) and not(self.identified((i, j-1))):
                     list_of_cells.append((i, j-1))                    
-                if self.withinboard(i, j+1) and not(self.identified((i, j+1))):
+                if self.withinboard((i, j+1)) and not(self.identified((i, j+1))):
                     list_of_cells.append((i, j+1))      
-                if self.withinboard(i-1, j-1) and not(self.identified((i-1, j-1))):
+                if self.withinboard((i-1, j-1)) and not(self.identified((i-1, j-1))):
                     list_of_cells.append((i-1, j-1))   
-                if self.withinboard(i-1, j) and not(self.identified((i-1, j))):
+                if self.withinboard((i-1, j)) and not(self.identified((i-1, j))):
                     list_of_cells.append((i-1, j))   
-                if self.withinboard(i-1, j+1) and not(self.identified((i-1, j+1))):
+                if self.withinboard((i-1, j+1)) and not(self.identified((i-1, j+1))):
                     list_of_cells.append((i-1, j+1))
             ns = Sentence(list_of_cells, count)
             self.knowledge.append(ns)  
