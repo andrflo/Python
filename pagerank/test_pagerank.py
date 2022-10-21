10517c316e9fb71b148b138c966d68903af4b58e
andrflo <andrewflorezrod@gmail.com>
2022-10-21 03:40:37 +0000

    Oct 20, 2022, 10:40 PM

--- pagerank/test_pagerank.py
+++ pagerank/test_pagerank.py
@@ -4,17 +4,36 @@ from pagerank import crawl
 
 def test_transition_model_corpus0():
     corpus = crawl("corpus0")   
-    ans =  transition_model(corpus, "1.html", 0.85)
+    ans = transition_model(corpus, "1.html", 0.85)
     assert ans == {"1.html": 0.0375, "2.html": 0.8875, "3.html": 0.0375, "4.html": 0.0375}
     acc = 0
     for k in ans:
         acc += ans[k]
     assert round(acc,0) == 1
-    assert transition_model(corpus, "2.html", 0.85) == {"1.html": 0.4625, "2.html": 0.0375, "3.html": 0.4625, "4.html": 0.0375}
-    assert transition_model(corpus, "3.html", 0.85) == {"1.html": 0.0375, "2.html": 0.4625, "3.html": 0.0375, "4.html": 0.4625}
-    assert transition_model(corpus, "4.html", 0.85) == {"1.html": 0.0375, "2.html": 0.8875, "3.html": 0.0375, "4.html": 0.0375}
-
+    ans = transition_model(corpus, "2.html", 0.85)
+    assert ans == {"1.html": 0.4625, "2.html": 0.0375, "3.html": 0.4625, "4.html": 0.0375}
+    acc = 0
+    for k in ans:
+        acc += ans[k]
+    assert round(acc,0) == 1
+    ans = transition_model(corpus, "3.html", 0.85)
+    assert ans == {"1.html": 0.0375, "2.html": 0.4625, "3.html": 0.0375, "4.html": 0.4625}
+    acc = 0
+    for k in ans:
+        acc += ans[k]
+    assert round(acc,0) == 1
+    ans = transition_model(corpus, "4.html", 0.85)
+    assert ans == {"1.html": 0.0375, "2.html": 0.8875, "3.html": 0.0375, "4.html": 0.0375}
+    acc = 0
+    for k in ans:
+        acc += ans[k]
+    assert round(acc,0) == 1
 
 def test_transition_model_corpus1():
     corpus = crawl("corpus1")    
-    assert transition_model(corpus, "bfs.html", 0.85) == {"bfs.html": 0.0214, "dfs.html": 0.0214, "games.html": 0.0214, "minesweeper.html": 0.0214, "minimax.html": 0.0214, "search.html": 0.8714, "tictactoe.html": 0.0214}    
+    ans = transition_model(corpus, "bfs.html", 0.85)
+    assert ans == {"bfs.html": 0.0214, "dfs.html": 0.0214, "games.html": 0.0214, "minesweeper.html": 0.0214, "minimax.html": 0.0214, "search.html": 0.8714, "tictactoe.html": 0.0214}    
+    acc = 0
+    for k in ans:
+        acc += ans[k]
+    assert round(acc,0) == 1
