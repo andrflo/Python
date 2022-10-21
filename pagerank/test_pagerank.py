6709464fd4e17d456f77b905c8a750fab48a820a
andrflo <andrewflorezrod@gmail.com>
2022-10-21 02:05:38 +0000

    Oct 20, 2022, 9:05 PM

--- pagerank/test_pagerank.py
+++ pagerank/test_pagerank.py
@@ -4,4 +4,4 @@ from pagerank import crawl
 
 def test_transition_model():
     corpus = crawl("corpus1")
-    assert transition_model()
+    assert transition_model(corpus, "1.html", 0.85) == {"1.html": None, "2.html": None, "3.html": None}
