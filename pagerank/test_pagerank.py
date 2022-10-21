55b7929d49d26174c0ac3dc1d262a97a8ceb85ef
andrflo <andrewflorezrod@gmail.com>
2022-10-21 03:20:57 +0000

    Oct 20, 2022, 10:20 PM

--- pagerank/test_pagerank.py
+++ pagerank/test_pagerank.py
@@ -10,6 +10,6 @@ def test_transition_model_corpus0():
     assert transition_model(corpus, "4.html", 0.85) == {"1.html": 0.0375, "2.html": 0.8875, "3.html": 0.0375, "4.html": 0.0375}
 
 
-#def test_transition_model_corpus1():
- #   corpus = crawl("corpus1")    
-  #  assert transition_model(corpus, "1.html", 0.85) == {"1.html": 0.0375, "2.html": 0.8875, "3.html": 0.0375, "4.html": 0.0375}    
+def test_transition_model_corpus1():
+    corpus = crawl("corpus1")    
+    assert transition_model(corpus, "bfs.html", 0.85) == {"bfs.html": 0.0375, "dfs.html": 0.8875, "games.html": 0.0375, "minesweeper.html": 0.0375, "minimax.html": 0.0375, "search.html": 0.0375, "tictactoe.html": 0.0375}    
