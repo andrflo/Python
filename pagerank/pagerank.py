716e4423eb53021161de7b78ea706e44e8c3d252
andrflo <andrewflorezrod@gmail.com>
2022-10-21 04:01:11 +0000

    Oct 20, 2022, 11:01 PM

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -84,7 +84,7 @@ def sample_pagerank(corpus, damping_factor, n):
     sample_pr[initial_page] += 1/n
     counter = n-1
     while counter>0:
-        
+        transition_model(corpus, initial_page, damping_factor)
     return sample_pr
 
 
