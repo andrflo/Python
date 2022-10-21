02735783dda130d79dbbf2933cd29fb8aba6ff71
andrflo <andrewflorezrod@gmail.com>
2022-10-21 02:38:08 +0000

    Oct 20, 2022, 9:38 PM

--- pagerank/test_pagerank.py
+++ pagerank/test_pagerank.py
@@ -7,5 +7,5 @@ def test_transition_model_corpus0():
     assert transition_model(corpus, "1.html", 0.85) == {"1.html": 0.8875, "2.html": 0.4625, "3.html": 0.4625, "4.html": 0.8875}
 
 def test_transition_model_corpus1():
-    corpus = crawl("corpus0")    
+    corpus = crawl("corpus1")    
     assert transition_model(corpus, "1.html", 0.85) == {"1.html": 0.8875, "2.html": 0.4625, "3.html": 0.4625, "4.html": 0.8875}    
