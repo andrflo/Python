7856ba4bfaa2c0a43d986c7cd4fb1a44b8a58d9b
andrflo <andrewflorezrod@gmail.com>
2022-10-23 05:27:34 +0000

    Oct 23, 2022, 12:27 AM

--- heredity/heredity.py
+++ heredity/heredity.py
@@ -246,7 +246,7 @@ def joint_probability(people, one_gene, two_genes, have_trait):
             #
             #
             
-            if people[person] in two_genes:
+            if not(people[person] in two_genes) and not(people[person] in one_gene):
                 # if the mother has zero copies of the gene
                 if (not(people[person]["mother"] in one_gene) and not(people[person]["mother"] in two_genes)):
                     # if the father has two copies of the gene
