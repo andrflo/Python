a32be6126884b707e4255a6a2b6c6c3f20f63d7b
andrflo <andrewflorezrod@gmail.com>
2022-10-23 19:10:50 +0000

    Oct 23, 2022, 2:10 PM

--- heredity/heredity.py
+++ heredity/heredity.py
@@ -234,13 +234,10 @@ def joint_probability(people, one_gene, two_genes, have_trait):
                     if (people[person]["father"] in two_genes):                        
                         prob_gene = (PROBS["mutation"])*(1-PROBS["mutation"])
                     # if the father has zero copies of the gene
-                    elif (not(people[person]["father"] in one_gene) and not(people[person]["father"] in two_genes)):
-                        # prob. that the father and the mother do not pass them
+                    elif (not(people[person]["father"] in one_gene) and not(people[person]["father"] in two_genes)):                        
                         prob_gene = (1-PROBS["mutation"])*(1-PROBS["mutation"])
                     # if the father has only one copy of the gene
-                    else:
-                        # prob. that the father and the mother does not pass it and it does not mutate  
-                        # or prob. that the father passes it and it mutates and the prob that the mother does not pass it and it does not mutate        
+                    else:                        
                         prob_gene = (0.5*(1-PROBS["mutation"]))*(1-PROBS["mutation"]) + 0.5*(PROBS["mutation"])*(1-PROBS["mutation"])  
                 # if the mother has two copies of the gene
                 if (people[person]["mother"] in two_genes):
