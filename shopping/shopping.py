5cbad3d2e402076c606b04d6e692194f57990368
andrflo <andrewflorezrod@gmail.com>
2022-10-28 17:50:14 +0000

    Oct 28, 2022, 12:50 PM

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -80,7 +80,7 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
 
 
 def evaluate(labels, predictions):
