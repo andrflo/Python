46025568ecbfd9b09d9da99366d4d86bbef2d92c
andrflo <andrewflorezrod@gmail.com>
2022-11-01 00:16:09 +0000

    Oct 31, 2022, 7:16 PM

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -83,20 +83,34 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    # Create a neural network
-    model = tf.keras.models.Sequential()
-    # Add a hidden layer with 8 units, with ReLU activation
-    model.add(tf.keras.layers.Dense(8, input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), activation="relu"))
-    # Add output layer with NUM_CATEGORIES units, with sigmoid activation
-    model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation="sigmoid"))
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(10, activation="softmax")
+    ])
 
     # Train neural network
     model.compile(
-        optimizer="adam",
-        loss="binary_crossentropy",
-        metrics=["accuracy"]
-    )
-    
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
     return model
 
 
