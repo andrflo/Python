d516b4459d155f41d571fc47157ebe440381f315
andrflo <andrewflorezrod@gmail.com>
2022-11-01 00:51:47 +0000

    Oct 31, 2022, 7:51 PM

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -88,7 +88,7 @@ def get_model():
 
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
-            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         # Max-pooling layer, using 2x2 pool size
