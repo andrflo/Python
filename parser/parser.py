10076c3d552430118a329a855b9cb4e35936797b
andrflo <andrewflorezrod@gmail.com>
2022-11-03 03:21:22 +0000

    Nov 2, 2022, 10:21 PM

--- parser/parser.py
+++ parser/parser.py
@@ -61,8 +61,7 @@ def preprocess(sentence):
     Pre-process sentence by converting all characters to lowercase
     and removing any word that does not contain at least one alphabetic
     character.
-    """
-    
+    """    
     return [x.lower() for x in nltk.tokenize.word_tokenize(sentence)]
 
 
