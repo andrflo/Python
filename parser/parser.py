ab84086812cde91801e8ec647ec7fd9cfd2790ce
andrflo <andrewflorezrod@gmail.com>
2022-11-03 22:22:41 +0000

    Nov 3, 2022, 5:22 PM

--- parser/parser.py
+++ parser/parser.py
@@ -83,6 +83,7 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
+    print(type(tree))
     print(tree)
 
 if __name__ == "__main__":
