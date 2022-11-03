0c962d5663b6b3f75625df4e526fd4488f70750a
andrflo <andrewflorezrod@gmail.com>
2022-11-03 03:08:48 +0000

    Nov 2, 2022, 10:08 PM

--- parser/parser.py
+++ parser/parser.py
@@ -62,7 +62,8 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    nltk.tokenize.word_tokenize(sentence)
+    
 
 
 def np_chunk(tree):
