f7b3a3310040acaf4f38020a60a04da7950655dd
andrflo <andrewflorezrod@gmail.com>
2022-11-03 15:58:55 +0000

    Nov 3, 2022, 10:58 AM

--- parser/parser.py
+++ parser/parser.py
@@ -16,7 +16,7 @@ V -> "smiled" | "tell" | "were"
 
 NONTERMINALS = """
 S -> NP VP | NP Conj VP | S Conj S
-NP -> Det NP | P NP | Det NP P NP | Det NP P Det NP | P Det NP | N | Adj N | NP
+NP -> Det NP | P NP | Det NP P NP | Det NP P Det NP | P Det NP | N | AN N | NP
 AN -> Adj | Adj AN
 VP -> V | Adv VP | V NP | VP Adv | VP
 """
