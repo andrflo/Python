98459747dea23a20347e3d1386649a6ddf2980a2
andrflo <andrewflorezrod@gmail.com>
2022-11-03 23:45:56 +0000

    Nov 3, 2022, 6:45 PM

--- parser/parser.py
+++ parser/parser.py
@@ -97,6 +97,7 @@ def containNP(tree):
         return False
     for s in tree.subtrees(lambda t: t.label() == "NP"):
         return True
+    print(tree.leaves())    
     return False    
 
 if __name__ == "__main__":
