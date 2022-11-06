dfb2d816d968e38b615ebe6fa2a72ce112ad0b72
andrflo <andrewflorezrod@gmail.com>
2022-11-06 17:56:16 +0000

    Nov 6, 2022, 12:56 PM

--- project/project.py
+++ project/project.py
@@ -398,7 +398,9 @@ def trafficLightIndication(dataset, dataoil):
             ">6µm (ISO)",
             ">14µm (ISO)",
             "Wasser K.F."
-        ):
+        )and (dataset.keys_exist(
+            "Datum letzter Ölwechsel", "Datum Probenentnahme"
+        ) or dataset.keys_exist("Einfülltage")):
             
             for row in reader:
                 if (
