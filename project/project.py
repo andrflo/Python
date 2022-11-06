53db88f1b25690cdf1adf1919496729a1d6ceac2
andrflo <andrewflorezrod@gmail.com>
2022-11-06 03:53:02 +0000

    Nov 5, 2022, 10:53 PM

--- project/project.py
+++ project/project.py
@@ -194,7 +194,7 @@ def get_model_idOil(numOils):
         tf.keras.layers.Dense(256, input_shape=(7,), activation="sigmoid"),           
 
         # Add a hidden layer 
-        tf.keras.layers.Dense(128, activation="sigmoid"),  
+        tf.keras.layers.Dense(128, activation="selu"),  
 
             
         # Add an output layer with NUM_CATEGORIES output units
