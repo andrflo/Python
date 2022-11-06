36adf772ea460455519df3e750037de4782f3aae
andrflo <andrewflorezrod@gmail.com>
2022-11-06 03:32:23 +0000

    Nov 5, 2022, 10:32 PM

--- project/project.py
+++ project/project.py
@@ -191,10 +191,10 @@ def get_model_idOil(numOils):
     model = tf.keras.models.Sequential([   
 
         # Add a hidden layer with x units, with ReLU activation
-        tf.keras.layers.Dense(256, input_shape=(7,), activation="sigmoid"),     
+        tf.keras.layers.Dense(5, input_shape=(7,), activation="sigmoid"),     
 
         # Add a hidden layer 
-        tf.keras.layers.Dense(128, activation="sigmoid"),               
+        tf.keras.layers.Dense(15, activation="sigmoid"),               
         
         # Add an output layer with NUM_CATEGORIES output units
         tf.keras.layers.Dense(numOils, activation="softmax")
