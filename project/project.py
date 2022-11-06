bac7ed976fddf391716864a99a12f0af25b0cfce
andrflo <andrewflorezrod@gmail.com>
2022-11-06 19:22:19 +0000

    Nov 6, 2022, 2:22 PM

--- project/project.py
+++ project/project.py
@@ -337,8 +337,8 @@ def trafficLightIndication(dataset, dataoil):
                     and row["SI"].isnumeric()
                     and row["K"].isnumeric()
                     and row["NA"].isnumeric()
-                    and row["Viskosität bei 40°C"] != ""
-                    and row["Viskosität bei 100°C"] != ""
+                    #and row["Viskosität bei 40°C"] != ""
+                    #and row["Viskosität bei 100°C"] != ""
                     and row["Oxidation"].isnumeric()
                     and row["CA"].isnumeric()
                     and row["MG"].isnumeric()
