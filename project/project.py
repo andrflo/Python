c03e1731ff5e4f8feb4975165b47162e1f86f0f2
andrflo <andrewflorezrod@gmail.com>
2022-11-06 02:43:03 +0000

    Nov 5, 2022, 9:43 PM

--- project/project.py
+++ project/project.py
@@ -179,7 +179,35 @@ def identifyWindTurbineOil(dataset, dataoil):
     model = get_model_idOil()
 
 def get_model_idOil():
-    ...
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),      
+
+        # Add an output layer with NUM_CATEGORIES output units
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    # Train neural network
+    model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+    return model
 
 
 
