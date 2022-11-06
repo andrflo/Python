fc6b24f31f8ed09214988739e0b2248afa9b527b
andrflo <andrewflorezrod@gmail.com>
2022-11-06 03:04:28 +0000

    Nov 5, 2022, 10:04 PM

--- project/project.py
+++ project/project.py
@@ -191,11 +191,11 @@ def get_model_idOil(numOils):
     model = tf.keras.models.Sequential([        
 
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="sigmoid"),
+        tf.keras.layers.Dense(256, activation="sigmoid"),
         #tf.keras.layers.Dropout(0.5), 
         
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(32, activation="sigmoid"),
+        tf.keras.layers.Dense(16, activation="sigmoid"),
         #tf.keras.layers.Dropout(0.5), 
            
         
