9b68122e97f9067232336d70dd462e2dd09bdad9
andrflo <andrewflorezrod@gmail.com>
2023-01-04 20:21:36 +0000

    Jan 4, 2023, 3:21 PM

--- project/project.py
+++ project/project.py
@@ -50,9 +50,10 @@ def main():
         # ds.plot_data_machine("time", "Viskosität bei 100°C")
         # ds.plot_data_machine("time", "FE")
         # ds.plot_data_machine("time", "P")
-        ds.plot_data_machine("time", "ZN")
+        # ds.plot_data_machine("time", "ZN")
         # ds.plot_data_machine("FE", "Neutralisationszahl")
         # ds.plot_data_machine("CU", "Neutralisationszahl")
+        ds.plot_data_machine("ZN", "Neutralisationszahl")
         # ds.plot_data_machine("Neutralisationszahl")
         # ds.plot_data_machine("Anlagengöße [kW]", "Ölmenge im System")
 
